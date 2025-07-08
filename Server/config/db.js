d368886cc8b2450b5ae58a7ea5612fefed6b8476
Tanvir <tanvirxng01@gmail.com>
2025-07-09 00:31:48 +0530

    Url updated

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/inventoryDB');
+    await mongoose.connect(process.env.MONGO_URI );
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection error:', err.message);
