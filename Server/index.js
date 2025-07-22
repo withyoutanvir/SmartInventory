238d739dca829cd11020b41b5f2a6ee2a7902cd5
Tanvir singh <113801504+withyoutanvir@users.noreply.github.com>
2025-07-22 21:52:53 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -42,5 +42,5 @@ app.get('/', (req, res) => {
 // Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
-  console.log(`✅ Server running at http://localhost:${PORT}`)
+  console.log(` Server running at http://localhost:${PORT}`)
 );
